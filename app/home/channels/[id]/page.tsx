cec1117f6cbcae16e71122038d60e7c69dd192c3
alan <alcapone@students.cs.ubc.ca>
2024-06-29 14:33:59 -0700

    fixed post

--- app/home/channels/[id]/page.tsx
+++ app/home/channels/[id]/page.tsx
@@ -8,9 +8,9 @@ export default async function Page({ params }: { params: { id: string } }) {
   return (
     <>
       <div className="flex flex-col py-5  w-2/3 h-auto">
-        <button className="btn btn-primary m-5 max-w-32">
-          <Link href={`./${params.id}/create`}>new post</Link>
-        </button>
+        <Link href={`./${params.id}/create`}>
+          <button className="btn btn-primary m-5 max-w-32">new post</button>
+        </Link>
         {/* <p>{pathname}</p> */}
         <Suspense fallback={<p>Loading...</p>}>
           <PostsWrapper channel={params.id} />
