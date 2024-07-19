87c5a2ef081e200d5b3b3eccd0f8a47954a8073f
alantensor <alanzhou318@gmail.com>
2024-07-18 22:13:19 -0700

    changed auth in header

--- app/ui/home/header.tsx
+++ app/ui/home/header.tsx
@@ -4,6 +4,18 @@ import { User } from "@supabase/supabase-js";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 
+const getURL = () => {
+  let url =
+    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    "http://localhost:3000/";
+  // Make sure to include `https://` when not localhost.
+  url = url.startsWith("http") ? url : `https://${url}`;
+  // Make sure to include a trailing `/`.
+  url = url.endsWith("/") ? url : `${url}/`;
+  return url;
+};
+
 export default function Header({
   user,
   setUser,
@@ -31,7 +43,7 @@ export default function Header({
       await supabase.auth.signInWithOAuth({
         provider: "google",
         options: {
-          redirectTo: `${process.env.REDIRECT_URL}/auth/callback`,
+          redirectTo: getURL(),
         },
       });
     } catch (error) {
